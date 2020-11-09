d2f5e870801c01d16c2618e2365bb34f467890ba
Jordan_Torres <jt.arontorres@gmail.com>
2020-11-09 00:03:46 -0800

    seccond commit, started adding code

---
+++ BankAccount.py
@@ -0,0 +1,8 @@
+class BankAccount:
+    def __init__(self, name, account, routing, balance):
+        self._name = name
+        self._account = account
+        self._routing = routing
+        self.balance = balance
+
+    

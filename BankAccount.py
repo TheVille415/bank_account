65050e6d8eb8c0c3f11ba622b69e063ae1d6e96a
Jordan_Torres <jt.arontorres@gmail.com>
2020-11-09 21:24:11 -0800

    fourth commit starting adding more methods

--- BankAccount.py
+++ BankAccount.py
@@ -15,4 +15,25 @@ class BankAccount:
         print(f"Routing No.: {self._routing}")
 
     def balance(self):
-        print(f"Balance: {self._balance}")
+        print(f"Balance: ${self._balance}")
+
+    def deposit(self, amount):
+        new_balance = amount + balance
+        print(f"Amount Deposited: ${new_balance}")
+
+    def add_interest(self, balance):
+        interest = balance *  0.00083
+        new_balance = interest + balance
+    
+    def print_receipt(self):
+        print(f"{self._name}")
+        print(f"Account No.: {self._account}")
+        print(f"Routing No.: {self._routing}")
+        print(f"Balance: ${self._balance}")
+
+
+
+
+jordan = BankAccount("Jordan", 12345678, 123456789, 1200)
+jordan.account_number()
+jordan.deposit(100)

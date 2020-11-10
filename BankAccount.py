d7e936dd55b0ac43f7b5428aba808f935533e4a0
Jordan_Torres <jt.arontorres@gmail.com>
2020-11-09 18:14:49 -0800

    third commit

--- BankAccount.py
+++ BankAccount.py
@@ -3,6 +3,16 @@ class BankAccount:
         self._name = name
         self._account = account
         self._routing = routing
-        self.balance = balance
+        self._balance = balance
 
-    
+    def full_name(self):
+        print(f"{self._name}")
+
+    def account_number(self):
+        print(f"Account No.: {self._account}")
+
+    def routing_number(self):
+        print(f"Routing No.: {self._routing}")
+
+    def balance(self):
+        print(f"Balance: {self._balance}")

abcd445cf248f159402c56ca499c742c354f52b8
Jordan_Torres <jt.arontorres@gmail.com>
2020-11-11 20:26:41 -0800

    sixth commit, fixed somethings and read the requiremnts for the program over

--- BankAccount.py
+++ BankAccount.py
@@ -1,53 +1,46 @@
+import random
 class BankAccount:
-    def __init__(self, name, account, routing, balance):
-        self._name = name
-        self._account = account
-        self._routing = routing
-        self._balance = balance
+    routing_number = 121042882
 
-    def full_name(self):
-        print(f"{self._name}")
+    def __init__(self, name, balance):
+        self.account = random.randint(000000000, 999999999)
+        self.name = name
+        self.balance = balance
 
-    def account_number(self):
-        print(f"Account No.: {self._account}")
-
-    def routing_number(self):
-        print(f"Routing No.: {self._routing}")
-
-    def balance(self):
-        print(f"Balance: ${self._balance}")
+    def get_balance(self):
+        print(f"Balance: ${self.balance}")
 
     def print_receipt(self):
-        print(f"{self._name}")
-        print(f"Account No.: {self._account}")
-        print(f"Routing No.: {self._routing}")
-        print(f"Balance: ${self._balance}")
+        new_balance = str(self.balance)
+        last_char = new_balance[-4:]
+        print(f"{self.name}")
+        print(f"Account No.: XXXX{last_char}")
+        print(f"Routing No.: {self.routing_number}")
+        print(f"Balance: ${self.balance}")
 
     def deposit(self, amount):
-        new_balance = amount + self._balance
+        new_balance = amount + self.balance
         print(f"Amount Deposited: ${amount}")
         print(f"New Balance: ${new_balance}")
 
     def add_interest(self, balance):
-        interest = self._balance *  0.00083
+        interest = self.balance *  0.00083
 
     def withdrawl(self, amount):
-        if amount > self._balance:
+        if amount > self.balance:
             print(f"Insufficient funds")
-        elif amount <= self._balance:
-            withdrawl_ammount = self._balance - amount
+        elif amount <= self.balance:
+            withdrawl_ammount = self.balance - amount
             print(f"Amount Withdrawn: ${amount}")
             print(f"New Balance: ${withdrawl_ammount}")
 
-jordan = BankAccount("Jordan Torres", 12345678, 123456789, 1200)
-
-jacob = BankAccount("Jacob Abrahams", 98765432, 192837465, 4000)
-
-brittany = BankAccount("Brittany Gilmore", 13579246, 246809753, 10000)
+#Users
+jordan = BankAccount("Jordan Torres", 1200)
+jacob = BankAccount("Jacob Abrahams", 4000)
+brittany = BankAccount("Brittany Gilmore", 10000)
 
 jordan.deposit(100)
-
 jacob.withdrawl(3000)
-
+jacob.print_receipt()
+brittany.withdrawl(3000)
 brittany.print_receipt()
-
